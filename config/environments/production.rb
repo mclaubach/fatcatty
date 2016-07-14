9a1d5daab418cb9f15baae45bedbaffac32edb8f
Matthew Laubach <mclaubach@gmail.com>
2016-07-14 17:34:19 +0000

    s3img

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,6 +6,16 @@ Rails.application.configure do
   config.assets.precompile += Ckeditor.assets
   config.assets.precompile += %w( *.js )
   config.assets.precompile += %w( .svg .eot .woff .ttf )
+  
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
+  }
+  }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
